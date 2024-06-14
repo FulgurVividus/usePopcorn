f23ab65b03cfa8292bd022eb42c1bbc1fd20c13e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-14 14:17:28 +0500

    chore: removed unnecessary comment

--- src/App.js
+++ src/App.js
@@ -68,7 +68,6 @@ export default function App() {
         <Box
           element={
             <>
-              {" "}
               <WatchedSummary watched={watched} />
               <WatchedMoviesList watched={watched} />
             </>

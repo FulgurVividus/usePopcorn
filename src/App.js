2d6c0193d4c72bda768e4ae84fe5630877654e96
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-17 16:38:12 +0500

    feat: added loading state

--- src/App.js
+++ src/App.js
@@ -56,17 +56,19 @@ const KEY = `38c94ee0`;
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   // Data fetching
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -91,15 +93,22 @@ export default function App() {
           }
         /> */}
 
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
         </Box>
       </Main>
+    </>
+  );
+}
+
+// Loader component
+function Loader() {
+  return (
+    <>
+      <p className="loader">Loading...</p>
     </>
   );
 }

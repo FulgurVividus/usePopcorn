446d8336cf3323222cfd22f6cd6f82622b480477
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-19 16:21:17 +0500

    feat: selecting a movie feature

--- src/App.js
+++ src/App.js
@@ -59,7 +59,15 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const tempQuery = "interstellar";
+  const [selectedId, setSelectedId] = useState(null);
+
+  const handleSelectMovie = function (id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  };
+
+  const handleCloseMovie = function () {
+    setSelectedId(null);
+  };
 
   // Data fetching
   useEffect(
@@ -125,13 +133,24 @@ export default function App() {
         <Box>
           {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
           {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList movies={movies} />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
           {error && <ErrorMessage message={error} />}
         </Box>
 
         <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          )}
         </Box>
       </Main>
     </>
@@ -267,12 +286,16 @@ function WatchedBox() {
 */
 
 // MovieList component
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
   return (
     <>
-      <ul className="list">
+      <ul className="list list-movies">
         {movies?.map((movie) => (
-          <Movie movie={movie} key={movie.imdbID} />
+          <Movie
+            movie={movie}
+            key={movie.imdbID}
+            onSelectMovie={onSelectMovie}
+          />
         ))}
       </ul>
     </>
@@ -280,10 +303,10 @@ function MovieList({ movies }) {
 }
 
 // Movie component
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
   return (
     <>
-      <li>
+      <li onClick={() => onSelectMovie(movie.imdbID)}>
         <img src={movie.Poster} alt={`${movie.Title} poster`} />
         <h3>{movie.Title}</h3>
         <div>
@@ -293,6 +316,20 @@ function Movie({ movie }) {
           </p>
         </div>
       </li>
+    </>
+  );
+}
+
+// MovieDetails component
+function MovieDetails({ selectedId, onCloseMovie }) {
+  return (
+    <>
+      <div className="details">
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        {selectedId}
+      </div>
     </>
   );
 }
